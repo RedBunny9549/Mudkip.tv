ef709a7a7c47a97da2e13288cb00968587667f16
RedBunny9549 <125116292+RedBunny9549@users.noreply.github.com>
2026-04-09 21:41:19 -0400

    t

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Player from './Player.jsx'
-import MangaReader from './MangaReader.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,15 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-<<<<<<< HEAD
-=======
-
-        {/* The Video Player Page */}
->>>>>>> 08e85babf28bd124342ac3f8f4e89b03d9a99aad
         <Route path="/watch/:id" element={<Player />} />
-
-        {/* The Manga Reader Page */}
-        <Route path="/read/:id" element={<MangaReader />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
